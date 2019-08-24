7b5a92eb5ffcd6703c61b24abccd7d8fc3bbe06b
jiro4989 <jiroron666@gmail.com>
2019-08-25 03:46:05 +0900

    :recycle: 変数名を修正

--- unicode/diacrit_test.go
+++ unicode/diacrit_test.go
@@ -12,16 +12,16 @@ func TestValidateDiaCriticalMark(t *testing.T) {
 		in      []rune
 		wantErr bool
 	}
-	tds := []TestData{
+	tests := []TestData{
 		{desc: "aは変換可能", in: []rune{'a'}},
 		{desc: "haは変換可能", in: []rune{'h', 'a'}},
 		{desc: "bは変換不可", in: []rune{'b'}, wantErr: true},
 	}
-	for _, v := range tds {
-		t.Run(v.desc, func(t *testing.T) {
-			err := ValidateDiaCriticalMark(v.in)
-			if v.wantErr {
-				assert.Error(t, err, v.desc)
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			err := ValidateDiaCriticalMark(tt.in)
+			if tt.wantErr {
+				assert.Error(t, err, tt.desc)
 			}
 		})
 	}
@@ -33,15 +33,15 @@ func TestToDiaCriticalMark(t *testing.T) {
 		in   []rune
 		want []rune
 	}
-	tds := []TestData{
+	tests := []TestData{
 		{desc: "aは変換可能", in: []rune{'a'}, want: []rune{'\u0363'}},
 		{desc: "haは変換可能", in: []rune{'h', 'a'}, want: []rune{'\u036A', '\u0363'}},
 		{desc: "bは変換不可", in: []rune{'b'}, want: []rune{0}},
 	}
-	for _, v := range tds {
-		t.Run(v.desc, func(t *testing.T) {
-			got := ToDiaCriticalMark(v.in)
-			assert.Equal(t, v.want, got, v.desc)
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			got := ToDiaCriticalMark(tt.in)
+			assert.Equal(t, tt.want, got, tt.desc)
 		})
 	}
 }
