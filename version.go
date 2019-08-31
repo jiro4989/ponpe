1e12a30bc3f3da5d69cbd30c551f4356a9557853
jiro4989 <jiroron666@gmail.com>
2019-08-31 18:41:19 +0900

    version up

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	Version = `ponpe version 1.0.1
+	Version = `ponpe version 1.0.2
 Copyright (C) 2019, jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/ponpe`
