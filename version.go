49a704fc2a06bbba10df7beba1818d6da5df482f
jiro4989 <jiro4989>
2022-05-23 08:20:35 +0900

    fix: bump major

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	Version = `ponpe version 1.1.0
+	Version = `ponpe version 2.0.0
 Copyright (C) 2019, jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/ponpe`
