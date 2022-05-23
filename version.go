9db925f75abf437c59109eb4918bead5767680b5
jiro4989 <jiro4989>
2022-05-23 09:09:24 +0900

    fix

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	Version = `ponpe version 1.2.0
+	Version = `ponpe version 1.3.2
 Copyright (C) 2019, jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/ponpe`
